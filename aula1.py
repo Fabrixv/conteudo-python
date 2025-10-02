73b2b42533848e0286f499782a5f6dc613113cfa
CAUÃ VINÍCIUS FABRIS GONÇALVES <caua_v_goncalves@estudante.sesisenai.org.br>
2025-10-02 16:48:31 -0300

    conteudo sobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,13 @@
+# como nosso primeiro codigo , vamos escapar da maldição
+print("hello word")
+# abaixo esta o comando print com sep end padrao
+print("logica","de","programação",sep="\n",end="\n")
+# abaixo cada concatenaçao e um enter (\n siginifica enter)
+print("vai esprimir em bixo ? ",'sep=\n')
+print("vai",end="\n")
+print("ai",end="\n")
+print("ai",end="\n")
+print("ai",end="\n")
+print("vai",end="\n")
+print("vai",end="\n")
+
---
+++ aula_variaveis.py
@@ -0,0 +1,13 @@
+nome= 'Carlao'
+print('o valor de nome é',nome)
+nome= 'creusa'
+print('o valor de nome é',nome)
+Nome= "Carlao"
+Salario = 1450.00
+print("O salario de ",Nome,"é de",Salario)
+nome= "adagoberto da silva pinto"
+salario = 1333
+dias_trabalhados= 13
+print("O funcionario é",nome)
+print("ele trabalhou",dias_trabalhados,"dias")
+print("O salarios é",salario)
