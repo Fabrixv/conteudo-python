1ff21c8a3f0c593ec0eddd7287ad996693eb5e0c
CAUÃ VINÍCIUS FABRIS GONÇALVES <caua_v_goncalves@estudante.sesisenai.org.br>
2025-10-30 16:51:45 -0300

    Exercios

---
+++ Exercicios 3/Ex2.py
@@ -0,0 +1,11 @@
+nome = input("Digite o seu nome:")
+while len (nome) < 3:
+    print("Nome invalido, ele precisa ter mais 3 caracteristicas")
+    nome = input("Digite o seu nome:")
+idade = int(input("digite sua idade"))
+while idade < 0 or idade > 150
+    print("Idade invalida")
+    salario = float(input("Digite o seu salario"))
+while salario < 0:
+    print('salario invalido')
+estado = input("Digite seu estado civil ")
